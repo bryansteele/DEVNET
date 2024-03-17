7df1bd539ff2ed80c2c367063baaa6c8de5fcde3
bryansteele <bryan@steele101>
2024-03-17 14:21:28 -0400

    *

---
+++ parsing_json/02.py
@@ -0,0 +1,12 @@
+import json
+import os
+from pprint import pprint
+
+here = os.path.abspath(os.path.dirname("/Users/bryan/dne-devfun-code/"
+                            "intro-python/parsing-json/nested_data.py"))
+
+with open(os.path.join(here, "/Users/bryan/dne-devfun-code/"
+                            "intro-python/parsing-json/nested_data.py")) as file:
+    json_text = file.read()
+
+print(json_text)
