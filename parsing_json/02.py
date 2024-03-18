b5278961a1a89e60886e9d990a01d64cf6ce9de4
bryansteele <bryan@steele101>
2024-03-18 00:08:03 -0400

    Parsing JSON Exercises Completed

--- parsing_json/02.py
+++ parsing_json/02.py
@@ -1,13 +1,16 @@
 import json
 import os
-from pprint import pprint
 
 here = os.path.abspath(os.path.dirname("/Users/bryan/dne-devfun-code/"
-                            "intro-python/parsing-json/nested_data.py"))
+                            "intro-python/parsing-json/interfaces.json"))
 
 with open(os.path.join(here, "/Users/bryan/dne-devfun-code/"
-                            "intro-python/parsing-json/nested_data.py")) as file:
-    json_text = file.read()
+                            "intro-python/parsing-json/interfaces.json")) as file:
+    json_text = json.loads(file.read())
 
-print(json_text) 
-
+for i in json_text["ietf-interfaces:interfaces"]["interface"]:
+    print("{name}: {ip} {netmask}".format(
+    name = i["name"],
+    ip = i["ietf-ip:ipv4"]["address"][0]["ip"],
+    netmask = i["ietf-ip:ipv4"]["address"][0]["netmask"],
+    ))
